fa3a8ff00555b7aada5fd8f5b815f64e97bd612c
Oleg <82386871+a374ru@users.noreply.github.com>
2023-04-11 15:22:00 +0300

    Ad link to id element ••• 2023

--- curyear.js
+++ curyear.js
@@ -1,3 +1,5 @@
 let li = document.querySelectorAll('li');
 dt = new Date().getFullYear();
-li[dt - 1].setAttribute('style', 'color:#e34234; border-left: 7px solid #000; font-size: 2em')
+li[dt - 1].setAttribute('style', 'color:#e34234; font-size: 2em');
+li[dt - 4].setAttribute("id", 'easter');
+document.location.replace('#easter');
