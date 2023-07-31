cd2862eb06242811c0134c3bb63a031ae030cf59
Oleg <82386871+a374ru@users.noreply.github.com>
2023-07-31 19:36:55 +0300

    Update curyear.js

--- curyear.js
+++ curyear.js
@@ -3,5 +3,8 @@ let li = document.querySelectorAll('li');
 dt = new Date().getFullYear();
 	li[dt - 1].setAttribute('style', 'color:#e34234; font-size: 1.5em');
 	li[dt - 4].setAttribute("id", 'easter');
-document.location.replace('#easter');
+	
+// document.location.replace('#easter');
+el = document.querySelector('#easter');
+el.scrollIntoView();
 });
