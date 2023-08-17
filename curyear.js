bfebbbd9bff87dbe4c21f57486bf202b7f0c4c5d
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-17 14:41:48 +0300

    Update curyear.js

--- curyear.js
+++ curyear.js
@@ -2,9 +2,12 @@ window.addEventListener("load", function () {
 let li = document.querySelectorAll('li');
 dt = new Date().getFullYear();
 	li[dt - 1].setAttribute('style', 'color:#e34234; font-size: 1.5em');
-	li[dt - 4].setAttribute("id", 'easter');
+	// li[dt].setAttribute('style', 'color:#fff; font-size: 1.7em; background-color: #e34234; padding: 12px;');
+	// li[dt].innerHTML += " <span style='color:#fca9a9; font-size: .7em; '>Ожидаемая Пасха</span>";
+	li[dt - 3].setAttribute("id", 'easter');
 	
 // document.location.replace('#easter');
 el = document.querySelector('#easter');
 el.scrollIntoView();
 });
+
