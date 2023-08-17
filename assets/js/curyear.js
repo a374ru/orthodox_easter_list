2b43aa82639e7d8928cce5f7094a4186dd184203
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-17 21:21:07 +0300

    333 ••• 2023 YSTM-MAC

--- assets/js/curyear.js
+++ assets/js/curyear.js
@@ -23,22 +23,15 @@ window.addEventListener("load", function () {
     mainElement = li[dt - 1];
     elementText = mainElement.textContent;
     dateElement = Number(elementText.match(/[0-9]*,/)[0].split(",")[0]);
-
     for (var el of arr) {
         if (elementText.search(el) > -1) {
             indexMontfElement = arr.findIndex((i) => i == el);
         }
     }
-
     easterelement = new Date(`${dt}/${indexMontfElement}/${dateElement}`);
-
     if (moment.getTime() > easterelement.getTime()) {
         li[dt - 1].setAttribute("class", "last-easter");
-
-        li[dt].setAttribute(
-            "class",
-            "easter"
-        );
+        li[dt].setAttribute("class", "easter");
         li[dt].innerHTML +=
             " <span style='color:#fca9a9; font-size: .7em; '>ожидаемая православная Пасха</span>";
     } else {
@@ -46,9 +39,7 @@ window.addEventListener("load", function () {
         li[dt - 1].innerHTML +=
             " <span style='color:#fca; font-size: .7em; '>ожидаемая православная Пасха</span>";
     }
-
-    li[dt - 3].setAttribute("id", "easter");
-
+    li[dt - 4].setAttribute("id", "easter");
     // document.location.replace('#easter');
     el = document.querySelector("#easter");
     el.scrollIntoView();
