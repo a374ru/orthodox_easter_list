a83cf57149a3dcbf7016f538e3a6d2eddc7d2499
Oleg <82386871+a374ru@users.noreply.github.com>
2023-08-22 19:37:34 +0300

    333 ••• 2023

--- curyear.js
+++ curyear.js
@@ -3,5 +3,5 @@ let li = document.querySelectorAll('li');
 dt = new Date().getFullYear();
 	li[dt - 1].setAttribute('style', 'color:#e34234; font-size: 1.5em');
 	li[dt - 4].setAttribute("id", 'easter');
-document.location.replace('#easter');
+	document.location.href('#easter');
 });
